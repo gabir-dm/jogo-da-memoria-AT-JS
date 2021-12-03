d2fb0f17a0443e9f2205eb69b922e7d21990452b
GDMelo <dmelo.gabriela@gmail.com>
2021-12-02 22:46:52 -0300

    refactor

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// 1. a pagina deve carregar com todas as cartas em pares desviradas
-// 2. ao clicar no botao iniciar jogo, o contador se inicia, as cartas embaralham, aparecem desviradas por 3 segundos e depois viram
-// 3. ao clicar em uma carta, ela deve desvirar e aguardar, caso uma segunda carta seja desvirada, deve-se testar a igualdade de ambas
-// 4. se for par, permanecem desviradas, senão viram de novo após 1,5s
-// 5. apos todas as cartas serem desviradas, o jogo termina
-// 6. apos desvirar uma segunda carta, nao devera ser possivel virar uma terceira
-// 7. ao termino do jogo, o contador para
-// 8. devera haver um botao de mostrar tempos de jogo anteriores
-
 let cards = ['dog1.png', 'dog1.png', 'dog2.png', 'dog2.png', 'dog3.png', 'dog3.png', 'dog4.png', 'dog4.png', 'dog5.png', 'dog5.png', 'dog6.png', 'dog6.png', 'dog7.png', 'dog7.png', 'dog8.png', 'dog8.png']
 let gameBoard = document.getElementById('tabuleiro')
 
@@ -37,6 +28,10 @@ function shuffleCards(){
             </div>`
     })
 
+    gameIsGo()
+}
+
+function gameIsGo(){
     let initialCards = document.getElementsByClassName('frontFace')
 
     setTimeout(() => {
