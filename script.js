0f9ca2a0a0275a25bf613ed877212b6d3170eeac
GDMelo <dmelo.gabriela@gmail.com>
2021-12-03 23:07:39 -0300

    added chronometer at game start

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let cards = ['dog1.png', 'dog1.png', 'dog2.png', 'dog2.png', 'dog3.png', 'dog3.png', 'dog4.png', 'dog4.png', 'dog5.png', 'dog5.png', 'dog6.png', 'dog6.png', 'dog7.png', 'dog7.png', 'dog8.png', 'dog8.png']
 let gameBoard = document.getElementById('tabuleiro')
+let blockClick = false
+
+let seconds = 0
+let minutes = 0
+let displaySeconds = 0
+let displayMinutes = 0
+let interval = null
 
 function gameStart(){
-    // let initialTime = new Date()
 
     gameBoard.innerHTML = ``
     cards.forEach(card => {
@@ -14,6 +20,26 @@ function gameStart(){
 
     setTimeout(() => {
         shuffleCards()}, 3000)
+}
+
+function startChrono(){
+    seconds++
+    if(seconds /60 === 1){
+        seconds = 0
+        minutes++
+    }
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString()
+    } else {
+        displaySeconds = seconds
+    }
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString()
+    } else {
+        displayMinutes = minutes
+    }
+
+document.getElementById("chrono").innerHTML = displayMinutes + ":" + displaySeconds
 }
 
 function shuffleCards(){
@@ -39,6 +65,10 @@ function gameIsGo(){
             let card = initialCards[i];
             card.classList.toggle('turned')
         }
+
+        interval = window.setInterval(startChrono, 1000)
+        startChrono()
+        
     }, 1500)
 
     let gameCard = document.querySelectorAll(".gameCard img")
